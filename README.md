ef1315e2ec7660a3e077f488af04a494132769b4
Esko Luontola <esko@luontola.fi>
2026-02-16 17:32:40 +0200

    Reproduce by navigating from page to page

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 1. Run `lein ring server-headless`
 2. Open http://localhost:3000/ in a browser and also open network dev tools
 3. Refresh the page tens of times
+   - Or navigate from page to page. Chromium seems to get stuck after just a few page navigations. Firefox takes longer.
 4. Eventually the `GET /` request gets stuck in pending state for tens of seconds  
--- src/repro/web.clj
+++ src/repro/web.clj
@@ -3,4 +3,19 @@
 (defn app [_]
   {:status 200
    :headers {"Content-Type" "text/html; charset=utf-8"}
-   :body "<!doctype html><html><head><title>ok</title></head><body>ok</body></html>"})
+   :body "<!doctype html>
+<html>
+<head>
+  <title>ok</title>
+</head>
+<body>
+  <p>ok</p>
+  <ul>
+    <li><a href='/page1'>page 1</a></li>
+    <li><a href='/page2'>page 2</a></li>
+    <li><a href='/page3'>page 3</a></li>
+    <li><a href='/page4'>page 4</a></li>
+    <li><a href='/page5'>page 5</a></li>
+  </ul>
+</body>
+</html>"})
